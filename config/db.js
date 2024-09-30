3290fff418758dda83460b961b8c79451d735c14
macn1 <athulraihan27@gmail.com>
2024-09-30 17:19:55 +0530

    db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host:process.env.DB_HOST,
-    dialect:'sqlite'
+const sequelize = new Sequelize({
+    
+    dialect:'sqlite',
+      storage: process.env.DB_STORAGE || './config/db.sqlite'
 })
 
 
